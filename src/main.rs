3d1fc22c1f52c46ab785b27dd4c8e6ef65c704d4
equal-l2 <equal-l2@users.noreply.github.com>
2017-01-23 10:39:22 +0900

    Adjust ProgressBar

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ use parameters::*;
 
 fn main(){
     let mut ps:[particle::Particle;PTCLNUM] = [Default::default();PTCLNUM];
-    let mut prog = progress_bar::ProgressBar::new((REP_N*PTCLNUM) as u64,50);
+    let mut prog = progress_bar::ProgressBar::new(REP_N as u64,50);
 
     let mut f = File::create("coord.dat").unwrap();
 
     for _ in 0..REP_N {
         for p in ps.iter_mut() {
             p.adv();
-            prog.adv();
             write!(&mut f, "{:e} {:e} {:e}\n",p.r[0],p.r[1],p.r[2]);
         }
+        prog.adv();
         write!(&mut f, "\n\n");
     }
 }
