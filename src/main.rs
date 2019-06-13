9c8847fc1b69370f08d638b78fab0b860e60a2bb
equal-l2 <eng.equall2@gmail.com>
2019-06-13 22:37:31 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 extern crate rand;
 use std::fs::File;
-use std::io::{stdout,Write};
-mod progress_bar;
+use std::io::{stdout, Write};
 mod parameters;
 mod particle;
+mod progress_bar;
 use parameters::*;
 
-fn main(){
-    let mut ps:[particle::Particle;PTCLNUM] = [Default::default();PTCLNUM];
-    let mut prog = progress_bar::ProgressBar::new(REP_N as u64,50);
+fn main() {
+    let mut ps: [particle::Particle; PTCLNUM] = [Default::default(); PTCLNUM];
+    let mut prog = progress_bar::ProgressBar::new(REP_N as u64, 50);
 
     let mut f = File::create("coord.dat").unwrap();
 
     for _ in 0..REP_N {
         for p in ps.iter_mut() {
             p.adv();
-            write!(&mut f, "{:e} {:e} {:e}\n",p.r[0],p.r[1],p.r[2]);
+            write!(&mut f, "{:e} {:e} {:e}\n", p.r[0], p.r[1], p.r[2]);
         }
         prog.adv();
         write!(&mut f, "\n\n");
     }
 }
-
